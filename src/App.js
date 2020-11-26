d3d89c7ff359030af419dc19d053a0ff9ec37ea2
Osama Zafar <37033400+osamakhanedu@users.noreply.github.com>
2020-11-26 16:24:28 +0500

    Bootstrap5 with custimzation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,45 @@
 import React from "react";
-import "../src/main.scss";
+import "./styles.scss";
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+    <div className="container mt-3">
+      <h2 class="text-center">Register</h2>
+      <div>
+        <div class="mb-3">
+          <label for="email" class="form-label">
+            Email address
+          </label>
+          <input
+            type="email"
+            class="form-control"
+            id="email"
+            placeholder="name@example.com"
+          />
+        </div>
+        <div class="mb-3">
+          <label for="password" class="form-label">
+            Password
+          </label>
+          <input
+            type="password"
+            class="form-control"
+            id="password"
+            placeholder="Password"
+          />
+        </div>
+        <div class="mb-3">
+          <label for="bio" class="form-label">
+            Tell us about you
+          </label>
+          <textarea class="form-control" id="bio" rows="3"></textarea>
+        </div>
+        <div class="col-12">
+          <button class="btn btn-primary" type="submit">
+            Register
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
